f31da384c21b8d5cfde6f17a0211ce84cde91e10
Lucie Zhong <luciez@Lucies-MacBook-Air.local>
2026-02-26 01:59:26 -0800

    hero animation

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,25 +2,121 @@ import fullLogoDark from "../assets/full_logo_dark.svg";
 import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import MotionPathPlugin from "gsap/MotionPathPlugin";
+
+import star_icon from "../assets/star.svg";
+import frontend_dev from "../assets/frontend.svg";
+import graphic_design from "../assets/graphic-design.svg";
+import uxui_design from "../assets/uxui.svg";
+import ux_research from "../assets/research.svg";
+import motion_graphics from "../assets/motion.svg";
+
+gsap.registerPlugin(MotionPathPlugin);
 
 function Hero() {
   const container = useRef();
 
   useGSAP(
     () => {
-      gsap.to("#orbit-path1", {
-        y: "-=10",
+      gsap.to("#star-small", {
+        motionPath: {
+          path: "#orbit2",
+          start: 0.1,
+          end: 1.1,
+        },
         transformOrigin: "50% 50%",
-        rotate: "-=4",
-        duration: 4,
+        duration: 20,
         repeat: -1,
-        yoyo: true,
-        ease: "sine.inOut",
+        ease: "none",
       });
 
-      gsap.to("#orbit-path2", {
-        y: "-=10",
+      gsap.to("#star-large", {
+        motionPath: {
+          path: "#orbit2",
+          start: 0.6,
+          end: 1.6,
+        },
         transformOrigin: "50% 50%",
+        duration: 20,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to("#planet", {
+        motionPath: {
+          path: "#orbit2",
+          start: 0.7,
+          end: 1.7,
+        },
+        transformOrigin: "50% 50%",
+        duration: 20,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to("#frontend", {
+        motionPath: {
+          path: "#orbit1",
+          start: -0.1,
+          end: 0.9,
+        },
+        transformOrigin: "50% 50%",
+        duration: 40,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to("#graphic", {
+        motionPath: {
+          path: "#orbit1",
+          start: 0.05,
+          end: 1.05,
+        },
+        transformOrigin: "50% 50%",
+        duration: 40,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to("#uxui", {
+        motionPath: {
+          path: "#orbit1",
+          start: 0.22,
+          end: 1.22,
+        },
+        transformOrigin: "50% 50%",
+        duration: 40,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to("#research", {
+        motionPath: {
+          path: "#orbit1",
+          start: 0.4,
+          end: 1.4,
+        },
+        transformOrigin: "50% 50%",
+        duration: 40,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to("#motion", {
+        motionPath: {
+          path: "#orbit1",
+          start: 0.52,
+          end: 1.52,
+        },
+        transformOrigin: "50% 50%",
+        duration: 40,
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to("#orbit1-group", {
+        y: "-=10",
+        transformOrigin: "400px 250px",
         rotate: "+=2",
         duration: 4,
         repeat: -1,
@@ -28,11 +124,56 @@ function Hero() {
         ease: "sine.inOut",
       });
 
-      gsap.to("#planet", {
+      gsap.to("#orbit2-group", {
         y: "-=10",
-        transformOrigin: "50% 50%",
+        transformOrigin: "400px 250px",
         rotate: "-=4",
         duration: 4,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to("#frontend", {
+        rotation: -15,
+        transformOrigin: "50% 50%",
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to("#graphic", {
+        rotation: 5,
+        transformOrigin: "50% 50%",
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to("#uxui", {
+        rotation: -5,
+        transformOrigin: "50% 50%",
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to("#research", {
+        rotation: 2,
+        transformOrigin: "50% 50%",
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.inOut",
+      });
+
+      gsap.to("#motion", {
+        rotation: -5,
+        transformOrigin: "50% 50%",
+        duration: 1,
         repeat: -1,
         yoyo: true,
         ease: "sine.inOut",
@@ -54,40 +195,73 @@ function Hero() {
           </p>
         </div>
         <svg
-          className="absolute w-full h-full pointer-events-none hidden sm:block z-1"
+          className="absolute w-full h-full pointer-events-none hidden sm:block"
           viewBox="0 0 800 500"
         >
-          <ellipse id="planet" cx="340" cy="112" rx="12" ry="12" fill="#FF9DBB" />
+          <g id="orbit2-group" transform="rotate(-14, 400, 250)">
+            <path
+              id="orbit2"
+              d="M70,250a330,150 0 1,0 660,0a330,150 0 1,0 -660,0"
+              fill="none"
+              stroke="rgba(255,255,255,0.7)"
+            />
+            <image
+              id="star-large"
+              href={star_icon}
+              width="66"
+              height="66"
+              x="-33"
+              y="-33"
+              transform="rotate(-5, -5, -5)"
+            />
+            <image
+              id="star-small"
+              href={star_icon}
+              width="60"
+              height="60"
+              x="-30"
+              y="-30"
+              transform="rotate(30, 30, 30)"
+            />
+            <ellipse id="planet" cx="0" cy="0" rx="12" ry="12" fill="#FF9DBB" />
+          </g>
         </svg>
         <svg
           className="absolute w-full h-full pointer-events-none hidden sm:block"
           viewBox="0 0 800 500"
         >
-          <ellipse
-            id="orbit-path1"
-            cx="400"
-            cy="250"
-            rx="330"
-            ry="150"
-            fill="none"
-            stroke="rgba(255,255,255,0.7)"
-            transform="rotate(-14, 400, 250)"
-          />
-        </svg>
-        <svg
-          className="absolute w-full h-full pointer-events-none hidden sm:block"
-          viewBox="0 0 800 500"
-        >
-          <ellipse
-            id="orbit-path2"
-            cx="400"
-            cy="250"
-            rx="360"
-            ry="160"
-            fill="none"
-            stroke="rgba(255,255,255,0.7)"
-            transform="rotate(-3, 400, 250)"
-          />
+          <g id="orbit1-group" transform="rotate(-3, 400, 250)">
+            <path
+              id="orbit1"
+              d="M40,250a360,160 0 1,0 720,0a360,160 0 1,0 -720,0"
+              fill="none"
+              stroke="rgba(255,255,255,0.7)"
+            />
+            <g id="frontend-wrapper">
+              <image
+                id="frontend"
+                href={frontend_dev}
+                width="150"
+                height="150"
+                x="-75"
+                y="-75"
+                transform="rotate(-8, -8, -8)"
+              />
+            </g>
+
+            <image id="graphic" href={graphic_design} width="110" height="110" x="-55" y="-55" />
+            <image id="uxui" href={uxui_design} width="150" height="150" x="-75" y="-75" />
+            <image id="motion" href={motion_graphics} width="134" height="134" x="-67" y="-67" />
+            <image
+              id="research"
+              href={ux_research}
+              width="120"
+              height="120"
+              x="-60"
+              y="-60"
+              transform="rotate(-5, -5, -5)"
+            />
+          </g>
         </svg>
       </div>
     </>
